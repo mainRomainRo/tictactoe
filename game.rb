a487d0f9deedca41d5d1f2d777fc6dabf8b99d91
mainRomainRo <romain.fiorotto@gmail.com>
2018-04-19 17:11:25 +0200

    pfffffff

--- game.rb
+++ game.rb
@@ -2,26 +2,29 @@ require_relative 'player'
 require_relative 'boardcase'
 require_relative 'board'
 
-
 class Game
 
 	def initialize
-	@board = Board.new
-	@player_1 = Player.new("X")
-    @player_2 = Player.new("O")
+	@@board = Board.new
+	@@player_1 = Player.new("X")
+    @@player_2 = Player.new("O")
 	end
 
-	def input(player_move)
-	input = [0, 1, 2, 3, 4, 5, 6, 7, 8]
+	def input(move, symbol)
+	input = []
+	@input[move] = symbol
+
 	end
 
 	def party
 	i=0
+	@@board
 
 	puts "\n------------------ Round number #{i+1} --------------------"
 	puts "Where are you going to play ? [1-9]"
     player_input = gets.chomp.to_i - 1
-    input[player_input] = player.symbol
+    puts @@board[player_input] = @@player_1.symbol
+    @@board.print_table
 
 end
 end
